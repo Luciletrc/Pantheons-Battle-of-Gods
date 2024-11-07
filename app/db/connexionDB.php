9a1413d7e9368316f613e64049f562761bdda0c5
Luciletrc <luciletrc@gmail.com>
2024-11-07 14:01:48 +0100

    Version 1.3

--- app/db/connexionDB.php
+++ app/db/connexionDB.php
@@ -6,7 +6,7 @@ $user = 'root';
 $pwd = '1234';
 $port = '3306';
 $charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
+$dsn = "pgsql:host=$host;dbname=$db;port=$port;charset=$charset";
 $options = [
     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
     \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
